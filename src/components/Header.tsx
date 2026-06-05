99c28d44c40071d6b1ddd1e974bf345070050ca8
jad <jadalouk@gmail.com>
2026-06-05 11:32:28 +0300

    ayre 1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,6 +13,8 @@ export const Header = () => {
     const debouncedSearch = useDebounce(search, 200)
     const suggestions = useQuery(api.product.searchKeywords, { searchTerm: debouncedSearch })
 
+    console.log(suggestions)
+
     return (
         <header className="w-full sticky top-0 z-50 border-b bg-background/70 backdrop-blur-sm">
             <div className="mx-auto max-w-7xl px-4 py-3 flex justify-between items-center gap-4 relative">
